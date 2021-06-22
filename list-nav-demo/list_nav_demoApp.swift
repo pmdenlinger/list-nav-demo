5bb358593e8e290b18bade1de4e31a1cc19551c3
Paul Denlinger <pmdenlinger@icloud.com>
2021-06-22 13:53:05 -0700

    Initial Commit

---
+++ list-nav-demo/list_nav_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  list_nav_demoApp.swift
+//  list-nav-demo
+//
+//  Created by user on 6/22/21.
+//
+
+import SwiftUI
+
+@main
+struct list_nav_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
